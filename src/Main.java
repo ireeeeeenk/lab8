2a62b985e3d0fa95c11072a33b2c90dbbccf08fb
ira <irinabortnikk@gmail.com>
2022-11-26 10:33:08 +0300

    code was changed

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
+    public static void main(String [] args) {
+        ArrayList<String> list = new ArrayList();
+        list.add("hello");
+        list.add("to");
+        list.add("everyone");
+
+
+
+
+        ArrayList<Integer> lengths = new ArrayList<>();
+
+        for (String word : list) {
+            lengths.add(word.length());
+        }
+        Collections.sort(lengths);
+        System.out.println(lengths);
+    }
 }
+
